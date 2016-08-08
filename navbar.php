7d320ea3a7301be0d0284d03bee6cb660d853d32
Matthew Stahlmann <matthew.stahlmann@gmail.com>
2016-08-08 03:03:09 -0400

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container full-width">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand">
+            <img src="images/BoksKar Name v4.0 Color.png" title="BOKSKar" alt="BOKSKar_logo" class="logo-bokskar" height="50px"/></a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse pull-right">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#" role="tab" data-toggle="tab">How it works</a></li>
+            <li style="display:none" class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                Menu <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Getting your vehicle</a></li>
+                  <li><a href="#">Maintenance and repairs</a></li>
+                  <li><a href="#">Upgrading to a new car</a></li>
+                  <li><a href="#">Downgrading to a new car</a></li>
+                  <li style="display: none;" role="separator" class="divider"></li>
+                  <li style="display: none;"  class="dropdown-header">Specials</li>
+                  <li><a href="#">Insurance acquisition</a></li>
+                </ul>
+            </li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#" role="tab" data-toggle="tab">FAQ</a></li>
+            <li><a href="#" role="tab" data-toggle="tab">Sign up <span style="display:none" class="glyphicon glyphicon-log-out"></span></a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#" role="tab" data-toggle="tab">Login</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#" role="tab" data-toggle="tab">About Us</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#" role="tab" data-toggle="tab">Support</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#" role="tab" data-toggle="tab">Blog</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
